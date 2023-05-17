feff23ac3169fa4147e777ee12561914d8751286
agustinSC2034 <aguustinn18@gmail.com>
2023-05-17 15:50:30 -0300

    17-5

---
+++ src/screens/products/index.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native/types";
+
+import React from "react";
+import {styles} from "./styles";
+
+const Products = () => {
+    return (
+        <View style = {styles.container}>
+            <Text>Products</Text>
+        </View>
+    )
+};
+
+export default Products
